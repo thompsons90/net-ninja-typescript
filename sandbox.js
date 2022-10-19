e2ceb4157245aba8d18b84b1b8c1cedcc855507e
Scott E Thompson <sthompson082290@gmail.com>
2022-10-19 08:08:50 -0500

    finished lesson 2

---
+++ sandbox.js
@@ -0,0 +1,6 @@
+var character = "Link";
+console.log(character);
+var inputs = document.querySelectorAll("input");
+inputs.forEach(function (input) {
+  console.log(input);
+});
---
+++ sandbox.ts
@@ -0,0 +1,11 @@
+const character = "Link";
+
+console.log(character);
+
+const inputs = document.querySelectorAll("input");
+
+console.log(inputs);
+
+inputs.forEach((input) => {
+  console.log(input);
+});
