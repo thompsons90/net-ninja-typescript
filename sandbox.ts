5444e8e7c1a995eb2ded07a8b5b2f4c4962b91f6
Scott E Thompson <sthompson082290@gmail.com>
2022-10-20 09:14:05 -0500

    finished lesson five

--- sandbox.ts
+++ sandbox.ts
@@ -1,11 +1,28 @@
-let character = "Link";
-let age = 30;
-let isBlackBelt = false;
+// explicit types
+// If we want to declare a variable as a type, but don't know its contents yet
+let character: string;
+let age: number;
+let isLoggedIn: boolean;
 
-character = "Luigi";
+age = "Luigi"; // can't give it a value of a different type
+isLoggedIn = true; // can fill the value in of the correct type
 
-const circ = (diameter: any) => {
-  return diameter * Math.PI;
-};
+// arrays
+let ninjas: string[]; // can only be an array of strings
+ninjas = [1, 2, 3]; // not allowed
+ninjas = ["Red", "Yellow", "Blue"]; // allowed
 
-console.log(circ(5));
+// sometimes we want to initialize it to be an empty array
+let vikings: string[];
+vikings.push("Axe throwin!"); // wouldn't be allowed because there's not an array yet
+
+let pirates: string[] = [];
+pirates.push("arrrrrgh!"); // in order to use push method
+
+// union types
+let mixed: (string | number | boolean)[] = []; // to allow multiple types w/empty array
+mixed.push("Hello");
+mixed.push(56);
+mixed.push(false);
+console.log(mixed);
+// objects
